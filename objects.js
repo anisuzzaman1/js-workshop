8773d0418ebb5aaf53bcc312a575e782f3b5ab3e
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-26 19:36:34 +0600

    Objects

---
+++ objects.js
@@ -0,0 +1,30 @@
+// Java Script Objects
+
+var student1 = {
+    id: 115,
+    name: "Anisuzzaman",
+    age: 40,
+    marks: 81
+};
+
+var mobile = {
+    color: 'Black',
+    price: 19000,
+    screenSize: '1000x1000',
+    storage: '16GB'
+};
+
+var book = {
+    title: 'Start with Y',
+    author: 'Symon',
+    price: 200.50,
+    publisher: 'Penguie'
+};
+
+// Print Out Objects Child
+console.log(book.title);
+
+// Update Objects Child
+book.title = "My New Author";
+
+console.log(book.title);
