2a4994df0a0e729ade2053746966734474d3b82f
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-26 15:43:04 +0600

    All example

---
+++ array.js
@@ -0,0 +1,7 @@
+var friend = [1, 2, 3, 4, 5];
+var lastBench = ['Alam', 'Balam', 'Kalam'];
+lastBench.push('Salam');
+var lastItem = lastBench.pop();
+
+// length(friend);
+console.log(lastBench);
---
+++ while-loop.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+var inc = 5;
+
+while (roastGiven < 7) {
+    // console.log(inc = roastGiven + 1)
+    console.log("Roast den please");
+    roastGiven++;
+}
