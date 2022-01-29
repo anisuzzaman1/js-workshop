09e50b5a4462d666b812c410b49de36ae43ed319
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-30 00:46:17 +0600

    Big Arrow

--- big-arrow.js
+++ big-arrow.js
@@ -1,3 +1,20 @@
+// Arrow Function
 const add = (a1, a2) => a1 + a2;
+// console.log(add(34, 23));
 
-console.log(add(34, 23));
+const getName = () => 'Anisuzzaman Md'
+const names = getName();
+console.log(names);
+
+
+// Multiline arrow function
+const doMath = (a1, a2) => {
+    const add = a1 + a2;
+    const div = a1 - a2;
+    const calc = add + div;
+    const result = calc * 5;
+    return result;
+}
+
+const total = doMath(5, 78);
+console.log(total);
