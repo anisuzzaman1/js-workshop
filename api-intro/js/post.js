2f17cf5870f4aa8d46ea685c41baf17fb495eed5
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-02-06 00:32:13 +0600

    Kanye Project

--- api-intro/js/post.js
+++ api-intro/js/post.js
@@ -19,3 +19,19 @@ function displayPost(posts) {
     }
 
 }
+
+function addPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'Submit from Dhaka',
+            body: 'Hello I am from Dhaka',
+            userId: 777,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
